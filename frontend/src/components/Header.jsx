a5695289053a8424752146ebeaa9481d5036bd90
Rahul Waghmare <rahulwaghmare1711@gmail.com>
2025-04-17 12:25:31 +0530

    Header Added

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,23 @@
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { FaShoppingCart, FaUser } from "react-icons/fa"
+
+const Header = () => {
+  return (
+  <header>
+    <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect >
+        <Container>
+            <Navbar.Brand >Proshop</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ms-auto">
+                    <Nav.Link to="/cart"><FaShoppingCart />Cart</Nav.Link>
+                    <Nav.Link to="/login"><FaUser />Sign In</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Container>
+    </Navbar>
+  </header>
+  );
+};
+
+export default Header;
